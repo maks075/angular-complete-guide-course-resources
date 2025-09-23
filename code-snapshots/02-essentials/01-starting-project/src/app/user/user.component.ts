9e87bf6497485c26b500dc2dc623da05ac1c11ac
maks075 <shlapak.maksym@gmail.com>
2025-09-23 15:18:24 -0500

    before practice code snapshot

---
+++ code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -0,0 +1,34 @@
+import { Component, computed, EventEmitter, Input, input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter<string>();
+
+
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imagePath = computed( () => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+
+
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
+}
